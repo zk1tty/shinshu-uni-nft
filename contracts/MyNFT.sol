a43e1527af70386ed0b703fe4b16535077225d19
zkitty <norika@0xcerberus.io>
2023-04-16 10:30:12 +0900

    add Ownable

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -3,10 +3,11 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage {
+contract MyNFT is ERC721URIStorage, Ownable {
     // TODO: need to count the totalSupply as counter
     // # Grammer: using A(library) for B(type, struct data type)
     using Counters for Counters.Counter;
